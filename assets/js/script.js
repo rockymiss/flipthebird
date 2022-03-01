fd85fd7a0ea696c0f90fb0609db0267f235785d7
rockymiss <rachelmcdermottrock@gmail.com>
2022-03-01 18:56:43 +0000

    Add variables to get section and flip lives and link to dom

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,7 @@ const detailsContainer = document.getElementById('details-container');
 const nameContainer = document.getElementById('name-container');
 const gameSelect = document.getElementById('game-select-container')
 const imagesQuiz = document.getElementById('quiz-container-images');
-const musicQuiz = document.getElementById('quiz-container-music');
-const shapesQuiz = document.getElementById('quiz-container-shapes');
+
 
 
 function getFormDetails(event) {
@@ -16,7 +15,15 @@ function getFormDetails(event) {
    
 }
 
-
-
 let getUserDetails = document.getElementById('name-form');
 getUserDetails.addEventListener('submit', getFormDetails);
+
+
+//Game 
+// variables to get parts of the document needed.
+const section = document.querySelector('section');
+const countLives = document.getElementById('flips');
+const setLives = 8;
+
+//Link flip lives variable to dom and set to setlives 
+countLives.textContent = `${setLives} flips left`;
