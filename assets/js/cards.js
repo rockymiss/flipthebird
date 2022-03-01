096fb66baa07eccd9598337f0e4934c3582c03c0
rockymiss <rachelmcdermottrock@gmail.com>
2022-03-01 21:14:59 +0000

    Add card generator function to script file and styles to section

---
+++ assets/js/cards.js
@@ -0,0 +1,20 @@
+//card data for bird images 
+
+const getImageData = () => [
+    {imgSrc: "https://res.cloudinary.com/rockymiss/image/upload/v1646161422/Flip%20the%20Bird/pexels-michelle-reeves-puffins_qa3amh.png", name: "puffins" },
+    {imgSrc: "https://res.cloudinary.com/rockymiss/image/upload/v1646161422/Flip%20the%20Bird/pexels-mark-broadhurst-colorfulparrot_lutlc4.png", name: "parrotcolor" },
+    {imgSrc: "https://res.cloudinary.com/rockymiss/image/upload/v1646161421/Flip%20the%20Bird/pexels-jad-el-mourad-red-bird_sgfwzw.png", name: "redparrot" },
+    {imgSrc: "https://res.cloudinary.com/rockymiss/image/upload/v1646161421/Flip%20the%20Bird/pexels-chris-clark-crested-lark_gty2tp.png", name: "lark" },
+    {imgSrc: "https://res.cloudinary.com/rockymiss/image/upload/v1646161422/Flip%20the%20Bird/pexels-nkk-inyourface_ue7mzj.png", name: "bird-face" },
+    {imgSrc: "https://res.cloudinary.com/rockymiss/image/upload/v1646161421/Flip%20the%20Bird/pexels-jean-van-der-meulen-peacock_s7mpi8.png", name: "peacock" },
+    {imgSrc: "https://res.cloudinary.com/rockymiss/image/upload/v1646161421/Flip%20the%20Bird/pexels-jos-van-ouwerkerk-bluehead_dzjett.png", name: "bluepeacock" },
+    {imgSrc: "https://res.cloudinary.com/rockymiss/image/upload/v1646161421/Flip%20the%20Bird/pexels-cristina-andrea-alvarez-cruz-hummingbird_agq8h0.png", name: "humming" },
+    {imgSrc: "https://res.cloudinary.com/rockymiss/image/upload/v1646161422/Flip%20the%20Bird/pexels-michelle-reeves-puffins_qa3amh.png", name: "puffins" },
+    {imgSrc: "https://res.cloudinary.com/rockymiss/image/upload/v1646161422/Flip%20the%20Bird/pexels-mark-broadhurst-colorfulparrot_lutlc4.png", name: "parrotcolor" },
+    {imgSrc: "https://res.cloudinary.com/rockymiss/image/upload/v1646161421/Flip%20the%20Bird/pexels-jad-el-mourad-red-bird_sgfwzw.png", name: "redparrot" },
+    {imgSrc: "https://res.cloudinary.com/rockymiss/image/upload/v1646161421/Flip%20the%20Bird/pexels-chris-clark-crested-lark_gty2tp.png", name: "lark" },
+    {imgSrc: "https://res.cloudinary.com/rockymiss/image/upload/v1646161422/Flip%20the%20Bird/pexels-nkk-inyourface_ue7mzj.png", name: "bird-face" },
+    {imgSrc: "https://res.cloudinary.com/rockymiss/image/upload/v1646161421/Flip%20the%20Bird/pexels-jean-van-der-meulen-peacock_s7mpi8.png", name: "peacock" },
+    {imgSrc: "https://res.cloudinary.com/rockymiss/image/upload/v1646161421/Flip%20the%20Bird/pexels-jos-van-ouwerkerk-bluehead_dzjett.png", name: "bluepeacock" },
+    {imgSrc: "https://res.cloudinary.com/rockymiss/image/upload/v1646161421/Flip%20the%20Bird/pexels-cristina-andrea-alvarez-cruz-hummingbird_agq8h0.png", name: "humming" },
+];
--- assets/js/script.js
+++ assets/js/script.js
@@ -27,3 +27,40 @@ const setLives = 8;
 
 //Link flip lives variable to dom and set to setlives 
 countLives.textContent = `${setLives} flips left`;
+
+
+
+// //function to make the cards run randomingly
+const randomCards = () => {
+    const cardInfo = getImageData();
+    cardInfo.sort(() => Math.random() - 0.5);
+    return cardInfo;
+};
+
+//Place cards into the html
+
+const cardGenerator = () => {
+    const cardInfo = randomCards();
+    //generate 16 cards
+    cardInfo.forEach(item => {
+        //get html
+    const cardElement = document.createElement('div');
+    const cardFace = document.createElement('img');
+    const cardBack = document.createElement('div');
+    //add class to cards
+    cardElement.classList = 'card';
+    cardFace.classList = 'face-card';
+    cardBack.classList = 'back-card';
+    //attach face to card 
+    cardFace.src = item.imgSrc;
+    //attach cards to section
+    section.appendChild(cardElement);
+    cardElement.appendChild(cardFace);
+    cardElement.appendChild(cardBack);
+    });   
+};
+
+cardGenerator();
+
+
+
