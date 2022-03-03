75282b0bbf80ee8b57f1bf540f60ccd36f6fc17e
rockymiss <rachelmcdermottrock@gmail.com>
2022-03-03 20:44:39 +0000

    Add audio data to card.js file

--- assets/js/cards.js
+++ assets/js/cards.js
@@ -19,3 +19,23 @@ const getImageData = () => [
     {imgSrc: "https://res.cloudinary.com/rockymiss/image/upload/v1646161421/Flip%20the%20Bird/pexels-cristina-andrea-alvarez-cruz-hummingbird_agq8h0.png", name: "humming" },
 ];
 
+const getAudioData = () => [
+    {audio="https://res.cloudinary.com/rockymiss/video/upload/v1646339721/Flip%20the%20Bird/sounds/es-seagull-squawks_rwokct.mp3", name: "seagull-squawks"},
+    {audio="https://res.cloudinary.com/rockymiss/video/upload/v1646339721/Flip%20the%20Bird/sounds/es-hawk-screech_mvusts.mp3", name: "hawk-screech"},
+    {audio="https://res.cloudinary.com/rockymiss/video/upload/v1646339721/Flip%20the%20Bird/sounds/es-bird-screech_n7fymk.mp3", name: "bird-screech"},
+    {audio="https://res.cloudinary.com/rockymiss/video/upload/v1646339721/Flip%20the%20Bird/sounds/es-bird-rooster_bbp0uc.mp3", name: "rooster"},
+    {audio="https://res.cloudinary.com/rockymiss/video/upload/v1646339721/Flip%20the%20Bird/sounds/es-bird-sandpiper_dumu5p.mp3", name: "sandpiper"},
+    {audio="https://res.cloudinary.com/rockymiss/video/upload/v1646339721/Flip%20the%20Bird/sounds/es-bird-goose-honk_ktlqcm.mp3", name: "goose-honk"},
+    {audio="https://res.cloudinary.com/rockymiss/video/upload/v1646339721/Flip%20the%20Bird/sounds/es-chicken-crow_coikhw.mp3", name: "chicken-crow"},
+    {audio="https://res.cloudinary.com/rockymiss/video/upload/v1646339720/Flip%20the%20Bird/sounds/es-bird-chirp_dz6tah.mp3", name: "bird-chirp"},
+    {audio="https://res.cloudinary.com/rockymiss/video/upload/v1646339721/Flip%20the%20Bird/sounds/es-seagull-squawks_rwokct.mp3", name: "seagull-squawks"},
+    {audio="https://res.cloudinary.com/rockymiss/video/upload/v1646339721/Flip%20the%20Bird/sounds/es-hawk-screech_mvusts.mp3", name: "hawk-screech"},
+    {audio="https://res.cloudinary.com/rockymiss/video/upload/v1646339721/Flip%20the%20Bird/sounds/es-bird-screech_n7fymk.mp3", name: "bird-screech"},
+    {audio="https://res.cloudinary.com/rockymiss/video/upload/v1646339721/Flip%20the%20Bird/sounds/es-bird-rooster_bbp0uc.mp3", name: "rooster"},
+    {audio="https://res.cloudinary.com/rockymiss/video/upload/v1646339721/Flip%20the%20Bird/sounds/es-bird-sandpiper_dumu5p.mp3", name: "sandpiper"},
+    {audio="https://res.cloudinary.com/rockymiss/video/upload/v1646339721/Flip%20the%20Bird/sounds/es-bird-goose-honk_ktlqcm.mp3", name: "goose-honk"},
+    {audio="https://res.cloudinary.com/rockymiss/video/upload/v1646339721/Flip%20the%20Bird/sounds/es-chicken-crow_coikhw.mp3", name: "chicken-crow"},
+    {audio="https://res.cloudinary.com/rockymiss/video/upload/v1646339720/Flip%20the%20Bird/sounds/es-bird-chirp_dz6tah.mp3", name: "bird-chirp"},
+
+
+
